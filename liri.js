982761acba60dde03459dcda3720e3d8b102e639
rizho123 <v-riczho@microsoft.com>
2019-07-30 04:56:39 -0700

    updating functions, fixing errors

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 var fs = require("fs")
 var request = require("request")
 var figlet = require("figlet")
+
+
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
@@ -66,15 +68,15 @@ request(queryUrl, function(error, response, body){
             var day = date.substring(8,10);
             var fullDate = month + "/" + day + "/" + year;
 
-            display(chalk.bgCyan("\n--------------------------BandsInTown--------------------------\n"))
-            display(chalk.cyan("Name: " + js[i].venue.name));
-            display(chalk.cyan("City: " + js[i].venue.city));
+            display(chalk.bgCyan.bold("\n--------------------------BandsInTown--------------------------\n"))
+            display(chalk.bgCyan.underline("Name:") + " " + chalk.cyan(js[i].venue.name));
+            display(chalk.bgCyan.underline("City:") + " " + chalk.cyan(js[i].venue.city));
             if (js[i].venue.region !== "") {
-                display(chalk.cyan("Country: " + js[i].venue.region));
+                display(chalk.bgCyan.underline("Country:") + " " + chalk.cyan(js[i].venue.region));
             }
-            display(chalk.cyan("Country: " + js[i].venue.country));
-            display(chalk.cyan("Date: " + fullDate));
-            display(chalk.bgCyan("\n--------------------------BandsInTown--------------------------\n"))
+            display(chalk.bgCyan.underline("Country:") + " " + chalk.cyan(js[i].venue.country));
+            display(chalk.bgCyan.underline("Date:") + " " + chalk.cyan(fullDate));
+            display(chalk.bgCyan.bold("\n--------------------------BandsInTown--------------------------\n"))
         }
     }
 })
@@ -89,7 +91,7 @@ function spotifySearch(parameter) {
         song = parameter;
     }
 
-    figlet(("SPOTIFY"), function(err,data) {
+    figlet("SPOTIFY", function(err,data) {
         if (err) {
             console.log(chalk.bgRed.yellow.bold("ERROR"))
             return;
@@ -105,12 +107,63 @@ function spotifySearch(parameter) {
             display(chalk.bgRed.yellow.bold("ERROR"))
             return;
         } else {
-            display(chalk.bgGreen("\n--------------------------Spotify--------------------------\n"))
-            display(chalk.green("Artist: " + data.tracks.items[0].artists[0].name))
-            display(chalk.green("Song: " + data.tracks.items[0].name))
-            display(chalk.green("Preview: " + data.tracks.items[3].preview_url));
-            display(chalk.green("Album: " + data.tracks.items[0].album.name))
-            display(chalk.bgGreen("\n--------------------------Spotify--------------------------\n"))
+            display(chalk.bgGreen.bold("\n--------------------------Spotify--------------------------\n"))
+            display(chalk.bgGreen.underline("Artist:") + " " + chalk.green(data.tracks.items[0].artists[0].name))
+            display(chalk.bgGreen.underline("Song:") + " " + chalk.green(data.tracks.items[0].name))
+            display(chalk.bgGreen.underline("Preview:") + " " + chalk.green(data.tracks.items[3].preview_url))
+            display(chalk.bgGreen.underline("Album:") + " " + chalk.green(data.tracks.items[0].album.name))
+            display(chalk.bgGreen.bold("\n--------------------------Spotify--------------------------\n"))
+        }
+    })
+}
+
+function omdbInfo(parameter) {
+    var movie;
+    if(parameter === undefined) {
+        movie = "Mr. Nobody";
+    } else {
+        movie = parameter;
+    }
+
+    figlet("OMDB", function(err, data) {
+        if (err) {
+            console.log(chalk.bgRed.yellow.bold("ERROR"))
+            return;
+        } 
+        console.log(chalk.magenta(data));
+    })
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function(err, response, body) {
+        var js = JSON.parse(body);
+        if(!err && response.statusCode === 200) {
+            display(chalk.bgMagenta.bold("\n--------------------------OMDB--------------------------\n"))
+            display(chalk.bgMagenta("Title:") + " " + chalk.magenta(js.Title))
+            display(chalk.bgMagenta("Release Year:") + " " + chalk.magenta(js.Year))
+            display(chalk.bgMagenta("IMDB Rating:") + " " + chalk.magenta(js.imdbRating))
+            display(chalk.bgMagenta("Rotten Tomatoes Rating:") + " " + chalk.magenta(js.Ratings[1].Value))
+            display(chalk.bgMagenta("Country:") + " " + chalk.magenta(js.Country))
+            display(chalk.bgMagenta("Language:") + " " + chalk.magenta(js.Language))
+            display(chalk.bgMagenta("Plot:") + " " + chalk.magenta(js.Plot))
+            display(chalk.bgMagenta("Actors:") + " " + chalk.magenta(js.Actors))
+            display(chalk.bgMagenta.bold("\n--------------------------OMDB--------------------------\n"))
+        }
+    })
+
+}
+
+function randomTxt() {
+    fs.readFile("random.txt", function(err,data) {
+        if (err) {
+            console.log(chalk.bgRed.yellow.bold("ERROR"))
+            return;
+        }
+
+        var query = data.split(",")
+        if (query[0] === "spotify-this-song") {
+            var song = query[1].trim().slice(1, -1)
+            spotifySearch(song);
         }
     })
 }
