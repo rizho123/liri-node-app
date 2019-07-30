3797da91061766ab96d428219a70e975c65c2fea
rizho123 <v-riczho@microsoft.com>
2019-07-29 22:25:12 -0700

    Initial setup

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
+var fs = require("fs")
+var request = require("request")
+var figlet = require("figlet")
 var keys = require("./keys.js");
-
+var Spotify = require("node-spotify-api")
 var spotify = new spotify(keys.spotify);
+
+var command = process.argv[2];
+var parameter = process.argv[3];
